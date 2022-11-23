526346433d7b1d2e95340c60c7c704e760bd28a1
Yuri E <196240@gmail.com>
2022-11-23 10:09:23 +0100

    ex2 added

---
+++ ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Input first number");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input second number");
+int M = Convert.ToInt32(Console.ReadLine());
+
+if (N > M){   
+    Console.WriteLine("MAX = " + N); 
+}  
+else
+{
+    Console.WriteLine("MAX = " + M); 
+}
