6a703c415ea8b70d2737049438333c9dc28b7d8d
Yuri E <196240@gmail.com>
2022-11-23 11:37:06 +0100

    ex4 added

---
+++ ex4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine ("Input first number");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input second number");
+int M = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input third number");
+int P = Convert.ToInt32(Console.ReadLine());
+
+if (N > M){   
+    if (N > P){
+        Console.WriteLine("MAX = " + N); 
+    } else {
+       Console.WriteLine("MAX = " + P);  
+        }
+}
+else if (M > P) {
+    Console.WriteLine("MAX = " + M);    
+}
+else {
+    Console.WriteLine("MAX = " + P);
+}    
